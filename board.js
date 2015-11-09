97e8d031cd09abc3bd3180eeec1264e818db7e0a
Axel Bayerl <axel.bayerl@gmail.com>
2015-11-08 21:13:13 -0300

    Primer Commit

---
+++ board.js
@@ -0,0 +1,160 @@
+
+var Board = function (sizex, sizey, bombs) {
+    this.sizex = sizex || 10;
+    this.sizey = sizey || 10;
+    this.bombs = bombs || 10;
+    this.checkboard = [];
+}
+
+Board.prototype.init_checkboard = function (selector) {
+    this.checkboard = [];
+    for(var i=0;i<this.sizey;i++) {
+        var tmp = [];
+        for(var j=0;j<this.sizex;j++) {
+            var new_cell = new Cell(j, i);
+            tmp.push(new_cell);
+        }
+        this.checkboard.push(tmp);
+    }
+
+    if(selector) {
+        $(selector).html("");
+        $(selector).append(this.get_table());
+        this.install_events();
+    }
+}
+
+Board.prototype.init_bombs = function () {
+    /*this.checkboard[0][0].has_bomb = true;
+    this.checkboard[1][1].has_bomb = true;
+    this.checkboard[2][2].has_bomb = true;
+    this.checkboard[3][3].has_bomb = true;
+    this.checkboard[4][4].has_bomb = true;
+    this.checkboard[5][5].has_bomb = true;
+    this.checkboard[6][6].has_bomb = true;
+    this.checkboard[7][7].has_bomb = true;
+    this.checkboard[8][8].has_bomb = true;
+    this.checkboard[9][9].has_bomb = true;*/
+
+    this.bombs = Math.min(this.bombs, this.sizey*this.sizex);
+
+    for(var i = 0 ; i < this.bombs ; i++) {
+        col = Math.floor(Math.random() * this.sizex);
+        row = Math.floor(Math.random() * this.sizey);
+
+        if(this.checkboard[row][col].has_bomb) i--;
+        this.checkboard[row][col].has_bomb = true;
+    }
+}
+
+Board.prototype.get_table = function () {
+    var a_table = $('<table>');
+
+    $.each(this.checkboard, function(rown, row) {
+        var one_row = $('<tr>');
+        $.each(row, function(coln, cell) {
+            one_row.append(cell.get_cell());
+        });
+        a_table.append(one_row.clone());
+    });
+
+    return a_table;
+}
+
+Board.prototype.install_events = function () {
+    $('body, html').contextmenu(function(ev) {
+        ev.preventDefault();
+        return false;
+    });
+    $(".cell-closed").mousedown(function (ev) {
+        ev.preventDefault();
+
+        start_watch();
+
+        var col = $(this).attr('data-col');
+        var row = $(this).attr('data-row');
+
+        switch (event.which) {
+            case 1:
+                board.reveal_cell(row, col);
+                break;
+            case 3:
+                board.flag_cell(row, col);
+                break;
+        }
+    })
+}
+
+Board.prototype.end_game = function () {
+    stop_watch();
+    $(".cell-closed").unbind('mousedown');
+}
+
+Board.prototype.reveal_cell = function (cell_row,cell_col) {
+    start_watch();
+    var res = this.checkboard[cell_row][cell_col].reveal();
+    switch(res) {
+    case 2: 
+        //GAME OVER
+        $.each(this.checkboard, function(row) {
+            $.each(this, function(col, val) {
+                if(val.has_bomb) val.reveal(true);
+                if(val.flagged) val.reveal(true);
+            });
+        });
+
+        this.end_game();
+    break;
+    case 3:
+        var arr = [[ 1, 0],[ 0, 1],
+                   [-1, 0],[ 0,-1],
+                   [ 1, 1],[-1,-1],
+                   [ 1,-1],[-1, 1]];
+
+        //Cuento cuantas bombas hay alrededor
+        var count = 0;
+
+        for(var i = 0 ; i < arr.length ; i++) {
+            var vec = arr[i];
+            new_col = parseInt(cell_col) + arr[i][1];
+            new_row = parseInt(cell_row) + arr[i][0];
+
+            if(typeof this.checkboard[new_row] !== 'undefined' && 
+               typeof this.checkboard[new_row][new_col] !== 'undefined' && 
+               this.checkboard[new_row][new_col].has_bomb)
+                count++;
+        }
+
+        //Si hay >= 1 lo muestro
+        if(count != 0) 
+            this.checkboard[cell_row][cell_col].set_text(count);
+
+        //Si no, sigo la recursividad
+        else
+            for(var i = 0 ; i < arr.length ; i++) {
+                new_col = parseInt(cell_col) + arr[i][1];
+                new_row = parseInt(cell_row) + arr[i][0];
+
+                if(typeof this.checkboard[new_row] !== 'undefined' && 
+                   typeof this.checkboard[new_row][new_col] !== 'undefined' &&
+                   !this.checkboard[new_row][new_col].revealed)
+                    this.reveal_cell(new_row, new_col);
+            };
+    break;
+    }
+
+    //Me fijo si ya termino el juego
+    var faltan = 0;
+    $.each(this.checkboard, function(row) {
+        $.each(this, function(col, val) {
+            if(!val.has_bomb && !val.revealed) faltan++;
+        });
+    });
+    if(faltan === 0)
+        this.end_game();
+}
+
+Board.prototype.flag_cell = function (cell_row,cell_col) {
+    var res = this.checkboard[cell_row][cell_col].flag();
+    return res;
+}
---
+++ cell.js
@@ -0,0 +1,91 @@
+var Cell = function (col,row,bomb) {
+    this.revealed = false;
+    this.flagged = false;
+    this.has_bomb = bomb || false;
+    this.classes = [];
+    this.update_classes();
+
+    this.col = col;
+    this.row = row;
+}
+
+Cell.prototype.get_id = function(_prefix) {
+    prefix = _prefix || "";
+    return prefix + 'cell_' + this.col + '_' + this.row;
+}
+
+Cell.prototype.get_cell = function() {
+    this.update_classes();
+
+    return $('<td class="'+this.get_classes()+'">')
+             .attr('id', this.get_id())
+             .attr('data-col', this.col)
+             .attr('data-row', this.row);
+}
+
+Cell.prototype.get_classes = function() {
+    return this.classes.join(" ");
+}
+
+Cell.prototype.update_classes = function() {
+    this.classes = ['cell'];
+
+    if(this.revealed) {
+        this.classes.push('cell-open');
+
+        if(this.flagged) {
+            if(this.has_bomb)
+                this.classes.push('fa', 'fa-check');
+            else
+                this.classes.push('fa', 'fa-close');
+        } else {
+            if(this.has_bomb)
+                this.classes.push('fa', 'fa-bomb');
+        }
+
+    } else {
+        this.classes.push('cell-closed');
+
+        if(this.flagged) this.classes.push('fa', 'fa-flag');
+    }
+}
+
+Cell.prototype.reveal = function(force) {
+    var make_force = force || false;
+
+    if(this.revealed) return 1;
+
+
+    if(!this.flagged || make_force) {
+        this.revealed = true;
+    } else return 1;
+
+    this.update_cell();
+
+    if(this.has_bomb) return 2;
+    return 3;
+}
+
+Cell.prototype.flag = function(force) {
+    if(!this.revealed) {
+        this.flagged = !this.flagged;
+    }
+
+    this.update_cell();
+}
+
+Cell.prototype.update_cell = function() {
+    this.update_classes();
+
+    $(this.get_id('#')).removeClass();
+    $(this.get_id('#')).addClass(this.get_classes());
+}
+
+Cell.prototype.set_text = function(_text) {
+    text = _text || "";
+
+    if(!this.has_bomb && !this.flagged)
+        $(this.get_id('#')).html(text);
+
+    return true;
+}
